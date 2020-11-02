3b67436148538770fce98707c62ef12acb111ad9
Roman <kinash0226@gmail.com>
2020-11-02 22:12:14 +0200

    first branch_02 commit

---
+++ Калькулятор/Калькулятор.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double a, b;
+
+    cout << "a=";
+    cin >> a;
+    do
+    {
+        cout << "b=";
+        cin >> b;
+        if (b == 0) cout << "" << endl;
+    } while (b == 0);
+    cout << a << " / " << b << " = " << a / b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    system("pause");
+    return 0;
+}
